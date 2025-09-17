dd0a9e6f99c0cbe435952d543539628a9a20a9ca
Amir-701 <taishy.aaa@gmail.com>
2025-09-17 15:52:05 +0500

    Update script.js
    Updated js with new cart

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let cartCount = 0;
+let cart = [];
 
-function addToCart() {
-  cartCount++;
-  document.getElementById('cart').innerText = `Корзина (${cartCount})`;
+function addToCart(name, price) {
+  cart.push({name, price});
+  document.getElementById('cart').innerText = `Корзина (${cart.length})`;
+  updateCartDisplay();
+}
+
+function toggleCart() {
+  const cartDiv = document.getElementById('cartItems');
+  cartDiv.style.display = cartDiv.style.display === 'none' ? 'block' : 'none';
+}
+
+function updateCartDisplay() {
+  const cartDiv = document.getElementById('cartItems');
+  if(cart.length === 0) {
+    cartDiv.innerHTML = 'Корзина пуста';
+  } else {
+    cartDiv.innerHTML = cart.map(item => `${item.name} - ${item.price} ₸`).join('<br>');
+  }
 }
